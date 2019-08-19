b89d0085cabc627f93c1f3b3ce29c2795bd7e414
Hyan <hyan.bueno@opngo.com.br>
2019-08-18 22:07:28 -0300

    ajust test FizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,12 +3,33 @@ class FizzBuzz {
 
     async multipleThree(value) {
         const result = value % 3
-        return result === 0 ?'Fizz' : value
+        return result === 0 ? 'Fizz' : value
     }
 
     async multipleFive(value) {
         const result = value % 5
         return result === 0 ? 'Buzz' : value
+    }
+
+    async multipleFiveAndThree(value) {
+        const fiveDivision = value % 5
+        const threeDivision = value % 3
+        if (threeDivision === 0 && fiveDivision === 0) return 'FizzBuzz'
+        return value
+    }
+
+    async pipeFizzBuzz(value) {
+        const twoValues = await this.multipleFiveAndThree(value)
+        if (twoValues !== value) return twoValues
+
+        const three = await this.multipleThree(value)
+        if (three !== value) return three
+
+        const fiveDivision = await this.multipleFive(value)
+        if (fiveDivision !== value) return fiveDivision
+
+        return value
+
     }
 
 }
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,37 +1,46 @@
 const FizzBuzz = require('./fizzbuzz.js')
 
-/**Escreva um pequeno programa que imprima cada número de 1 a 100 em uma nova linha.
+describe('[GIVEN] Para cada múltiplo de 3, imprima "Fizz" em vez do número \n'
+         + ' Para cada múltiplo de 5, imprima "Buzz" em vez do número \n'
+         + ' Para números que são múltiplos de 3 e 5, imprima "FizzBuzz" em vez do número.', () => {
 
-Para cada múltiplo de 3, imprima "Fizz" em vez do número.
-
-Para cada múltiplo de 5, imprima "Buzz" em vez do número.
-
-Para números que são múltiplos de 3 e 5, imprima "FizzBuzz" em vez do número. */
-
-describe('Hello TDD FizzBuz', () => {
-
-    test('For numbers multiples of three should print FIZZ', async () => {
+    test('[THEN] - For numbers multiples of three should print FIZZ', async () => {
         const result = await FizzBuzz.multipleThree(3)
         expect(result).toBe('Fizz')
     })
 
 
-    test('For numbers NOT multiples of three should print value', async () => {
+    test('[THEN] - For numbers NOT multiples of three should print value', async () => {
         const value = 5
         const result = await FizzBuzz.multipleThree(value)
         expect(result).toBe(value)
     })
 
-    test('For numbers multiples of five should print BUZZ', async () => {
+    test('[THEN] - For numbers multiples of five should print BUZZ', async () => {
         const result = await FizzBuzz.multipleFive(5)
         expect(result).toBe('Buzz')
     })
 
 
-    test('For numbers NOT multiples of three should print value', async () => {
+    test('[THEN] - For numbers NOT multiples of three should print value', async () => {
         const value = 6
         const result = await FizzBuzz.multipleFive(value)
         expect(result).toBe(value)
+    })
+
+
+    test('[THEN] - Multiple two Five And Three should print FIZZBUZZ', async () => {
+        const result = await FizzBuzz.multipleFiveAndThree(15)
+        expect(result).toBe('FizzBuzz')
+    })
+
+    test('[THEN] - Zero to fifteen shoud to list mock', async() => {
+        const mock = [1,2,'Fizz',4,'Buzz','Fizz',7,8,'Fizz','Buzz',11,'Fizz',13,14,'FizzBuzz']
+        const result = []   
+        for(i = 1 ; i <= 15 ; i++){
+            result.push(await FizzBuzz.pipeFizzBuzz(i))
+        }   
+        expect(result).toEqual(mock)
     })
 
 });
